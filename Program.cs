3a957f613c3277a088c008ea72e4ba2842a8aef4
Ana <analubari@gmail.com>
2023-04-20 02:02:04 -0300

    exercise 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape triangle = new Triangle(10,5, 4, 5);
+            Shape circle = new Circle(4);
+            Shape square = new Square(7);
+
+            triangle.GetArea();
+            triangle.Perimeter();
+            circle.GetArea();
+            circle.Perimeter();
+            square.GetArea();
+            square.Perimeter();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    public interface Shape
+    {   
+        double GetArea();
+        double Perimeter();
+    }
+}
